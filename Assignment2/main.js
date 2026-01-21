1e9ba7988dabdf87a2211d085ed577d1c358f4f1
varshneyakanksha <varshneyakanksha05@gmail.com>
2026-01-21 23:18:00 +0530

    Exercise 3: System information logger using os and fs

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -2,10 +2,16 @@
 
 // Exercise 1
 fileOps.readFile();
-fileOps.countWords();*/
-const stringUtils = require("./stringUtils");
+fileOps.countWords();
+const stringUtils = require("./CustomModule");
 
 // Exercise 2
 console.log(stringUtils.capitalize("hello world"));
 console.log(stringUtils.reverseString("nodejs"));
-console.log("Vowels:", stringUtils.countVowels("JavaScript"));
+console.log("Vowels:", stringUtils.countVowels("JavaScript"));*/
+
+const logSystemInfo = require("./SystemLogger");
+
+// Exercise 3
+logSystemInfo();
+
