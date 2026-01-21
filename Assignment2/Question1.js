117b0944580aeeae74f80e87871027ee3d9670a4
varshneyakanksha <varshneyakanksha05@gmail.com>
2026-01-21 22:19:05 +0530

    [Akanksha Varshney].add exercise1 FileOperation

---
+++ Assignment2/Question1.js
@@ -0,0 +1,34 @@
+let fs = require("fs");
+
+// read
+function readFile() {
+    fs.readFile("a.txt", "utf-8", (err, data) => {
+        if (err) throw err;
+        console.log("File data:");
+        console.log(data);
+    });
+}
+
+// count
+function countWords() {
+    fs.readFile("a.txt", "utf-8", (err, data) => {
+        if (err) throw err;
+
+        let words = data.split(" ");
+        let count = words.length;
+
+        console.log("Total words:", count);
+
+        // write
+        fs.writeFile("output.txt", "Total words: " + count, (err) => {
+            if (err) throw err;
+            console.log("Result written successfully");
+        });
+    });
+}
+
+// export functions
+module.exports = {
+    readFile,
+    countWords
+};
---
+++ Assignment2/main.js
@@ -0,0 +1,5 @@
+const fileOps = require("./Question1");
+
+// Exercise 1
+fileOps.readFile();
+fileOps.countWords();
