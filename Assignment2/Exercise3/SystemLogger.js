25e4c1bee46502b35db2d801a09aa5e79c681c0d
varshneyakanksha <varshneyakanksha05@gmail.com>
2026-01-22 14:36:19 +0530

    [Akanksha Varshney].add exercise3

---
+++ Assignment2/Exercise3/SystemLogger.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+const fs = require("fs");
+function logSystemInfo() {
+        setInterval(() => {
+                    let info = `
+CPU: ${os.cpus()[0].model}
+Total Memory: ${os.totalmem()}
+Platform: ${os.platform()}
+-----------------------
+`;
+        fs.appendFile("systemInfo.txt", info, (err) => {
+            if (err) throw err;
+            console.log("System info logged");
+        });
+    }, 6000);
+}
+module.exports = logSystemInfo;
+
+
+
+
