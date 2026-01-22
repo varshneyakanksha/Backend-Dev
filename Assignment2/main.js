d68c1a4d9ad38eba22308075d33c3e30ae7f4309
varshneyakanksha <varshneyakanksha05@gmail.com>
2026-01-22 14:41:13 +0530

    Remove old files after organizing into Exercise folders

--- Assignment2/main.js
+++ Assignment2/main.js
@@ -9,10 +9,15 @@ const stringUtils = require("./CustomModule");
 
 console.log(stringUtils.capitalize("hello world"));
 console.log(stringUtils.reverseString("nodejs"));
-console.log("Vowels:", stringUtils.countVowels("JavaScript"));*/
+console.log("Vowels:", stringUtils.countVowels("JavaScript"));
 
 // Exercise 3
 const logSystemInfo = require("./SystemLogger");
 
-logSystemInfo();
+logSystemInfo();*/
+
+import createStudent from "./createstudent.js";
+
+
+console.log(createStudent("satya",2,"mCA"));
 
