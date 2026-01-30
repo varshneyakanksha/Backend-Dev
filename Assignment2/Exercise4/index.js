a7e61b88483be35fa801e5ef206b19d3f50f767e
varshneyakanksha <varshneyakanksha05@gmail.com>
2026-01-30 21:11:20 +0530

    Remove Assignment2 from Git, keep locally

--- Assignment2/Exercise4/index.js
+++
@@ -1,9 +0,0 @@
-import { createTodo, readTodo, updateTodo, deleteTodo } from "./todoAPI.js"
-
-console.log(createTodo("Learn Node.js"))
-console.log(createTodo("Practice Assignment"))
-
-console.log(readTodo())
-
-console.log(updateTodo(0))
-console.log(deleteTodo(1))
