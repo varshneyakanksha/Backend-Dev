d171473ec883e93334d107446914409df03eda6e
varshneyakanksha <varshneyakanksha05@gmail.com>
2026-02-11 00:18:28 +0530

    [Akanksha Varshney].add question4

---
+++ question4/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Message:", message);
+
+  res.send("Form successfully submitted 👍");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
